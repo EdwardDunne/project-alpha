bea3bf85b8d13c214d0e22e1255417cf0f370b09
EJ Dunne <edunne@oncell.com>
2021-09-07 23:52:09 -0400

    Revert "updating docker-compose-prod"
    This reverts commit 476fd17b7218e7dba79c2dd71f9df7a564279fbf.

--- app/project_alpha/settings.py
+++ app/project_alpha/settings.py
@@ -129,5 +129,3 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
-
-SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
