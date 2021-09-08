d32cf2623f294d64614bf22de5bc59e85c25103f
EJ Dunne <edunne@oncell.com>
2021-09-08 15:32:23 -0400

    lets encrypt

--- app/project_alpha/settings.py
+++ app/project_alpha/settings.py
@@ -129,3 +129,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "mediafiles")
+
+SECURE_PROXY_SSL_HEADER = ("HTTP_X_FORWARDED_PROTO", "https")
