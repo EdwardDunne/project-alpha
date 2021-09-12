33c109a74beb81bb49286b368ce5239ea40ef6b8
EJ Dunne <edunne@oncell.com>
2021-09-12 00:22:40 -0400

    testing background parallax

--- app/frontend/src/components/HexMenu.js
+++ app/frontend/src/components/HexMenu.js
@@ -43,3 +43,21 @@ export default class HomePage extends Component {
         );
     }
 }
+
+(function() {
+    // Add event listener
+    // document.addEventListener("mousemove", parallax); TEMPORARILY REMOVED
+    const $main_background = $(".parallax");
+    function parallax(e) {
+        let _w = window.innerWidth/2;
+        let _h = window.innerHeight/2;
+        let _mouseX = e.clientX;
+        let _mouseY = e.clientY;
+        let _depth1 = `${50 - (_mouseX - _w) * -0.01}% ${50 - (_mouseY - _h) * 0.01}%`;
+        let _depth2 = `${50 - (_mouseX - _w) * 0.02}% ${50 - (_mouseY - _h) * 0.02}%`;
+        let _depth3 = `${50 - (_mouseX - _w) * 0.06}% ${50 - (_mouseY - _h) * 0.06}%`;
+        let x = `${_depth1}`;
+        $main_background.css('background-position', x);
+    }
+
+})();
