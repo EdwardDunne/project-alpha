46ef30e8d0b8008719fb4267013befa824f615de
Edward P Dunne Jr. <edunne05@gmail.com>
2026-03-28 09:25:59 -0400

    remove unused files

--- app/frontend/src/images/index.ts
+++ app/frontend/src/images/index.ts
@@ -2,8 +2,6 @@ import matrix_hex_img from './hex-images/matrix-hex-img.jpeg';
 import resume_hex_img from './hex-images/resume-hex-img.webp';
 import linkedin_hex_img from './hex-images/linkedin-hex-img.webp';
 import contact_hex_img from './hex-images/contact-hex-img.webp';
-import art_hex_img from './hex-images/art-hex-img.jpeg';
-import fb_hex_img from './hex-images/fb-hex-img.jpeg';
 import ig_hex_img from './hex-images/ig-hex-img.webp';
 import comics_hex_img from './hex-images/comics-hex-img.jpg';
 import tiktok_hex_img from './hex-images/tiktok-hex-img.jpg';
@@ -14,8 +12,6 @@ export {
     resume_hex_img,
     linkedin_hex_img,
     contact_hex_img,
-    art_hex_img,
-    fb_hex_img,
     ig_hex_img,
     comics_hex_img,
     tiktok_hex_img,
--- app/frontend/urls.py
+++ app/frontend/urls.py
@@ -3,7 +3,6 @@ from .views import index
 
 urlpatterns = [
     path('', index),
-    path('admin-test', index),
     path('comics-admin', index),
     path('login', index),
     path('register', index),
