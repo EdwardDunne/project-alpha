1c82d42d0c8d391303b09a2b1620a2908458f4f4
Edward P Dunne Jr. <edunne05@gmail.com>
2026-07-22 12:13:37 -0400

    Update ResumeModal.tsx

--- app/frontend/src/modals/ResumeModal.tsx
+++ app/frontend/src/modals/ResumeModal.tsx
@@ -1,37 +1,52 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from "react"
+import ReactDom from "react-dom"
 
 interface Props {
-    open: boolean;
-    children?: React.ReactNode;
-    onClose: () => void;
+    open: boolean
+    children?: React.ReactNode
+    onClose: () => void
 }
 
 export default function ResumeModal({ open, children, onClose }: Props) {
-
-    if (!open) return null;
-    const modal_title = 'Resume'
+    if (!open) return null
+    const modal_title = "Resume"
 
     return ReactDom.createPortal(
         <>
-            <div className='fixed inset-0 bg-black/[0.43] z-[1000]' onClick={onClose}/>
-            <div className='fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white z-[1001] rounded-[0.6rem] max-h-[90dvh] overflow-y-auto w-[92vw] px-[2rem] py-[2.5rem] md:w-[80vw] md:max-w-[106rem] md:h-[80vh] md:p-[2rem] flex flex-col flex-nowrap justify-between items-center'>
-                <h1 className='text-center w-full'>{modal_title}</h1>
-                <span className='text-center w-full h-[60vh] overflow-scroll'>
-                    <img src="https://drive.google.com/thumbnail?id=1486IJXRqbg4imoPuZxW6JDQVPuTHjbGd&sz=w1000" alt=""/>
-                    <img src="https://drive.google.com/thumbnail?id=1uxFUcBdgGHew7DUdjlfhkSBU02XdyKK3&sz=w1000" alt=""/>
+            <div
+                className="fixed inset-0 bg-black/[0.43] z-[1000]"
+                onClick={onClose}
+            />
+            <div className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white z-[1001] rounded-[0.6rem] max-h-[90dvh] overflow-y-auto w-[92vw] px-[2rem] py-[2.5rem] md:w-[80vw] md:max-w-[106rem] md:h-[80vh] md:p-[2rem] flex flex-col flex-nowrap justify-between items-center">
+                <h1 className="text-center w-full">{modal_title}</h1>
+                <span className="w-full h-[60vh]">
+                    <iframe
+                        src="https://drive.google.com/file/d/1pA9HMsiar6KLBY7l8xwGwx5LuUM3vttn/preview"
+                        title="Resume Preview"
+                        className="w-full h-full rounded"
+                    />
                 </span>
-                <span className='flex justify-end items-center w-full'>
+                <span className="flex justify-end items-center w-full">
                     <button
-                        className='px-5 py-2 mr-4 bg-brand text-white rounded hover:bg-brand-dark transition-colors font-semibold'
-                        onClick={() => { window.open("https://drive.google.com/u/1/uc?id=1486IJXRqbg4imoPuZxW6JDQVPuTHjbGd&export=download", "_blank"); }}
+                        className="px-5 py-2 mr-4 bg-brand text-white rounded hover:bg-brand-dark transition-colors font-semibold"
+                        onClick={() => {
+                            window.open(
+                                "https://drive.google.com/u/1/uc?id=1pA9HMsiar6KLBY7l8xwGwx5LuUM3vttn&export=download",
+                                "_blank",
+                            )
+                        }}
                     >
                         Download
                     </button>
-                    <button className='px-5 py-2 bg-brand text-white rounded hover:bg-brand-dark transition-colors font-semibold' onClick={onClose}>Close</button>
+                    <button
+                        className="px-5 py-2 bg-brand text-white rounded hover:bg-brand-dark transition-colors font-semibold"
+                        onClick={onClose}
+                    >
+                        Close
+                    </button>
                 </span>
             </div>
         </>,
-        document.getElementById('portal') as Element
+        document.getElementById("portal") as Element,
     )
 }
