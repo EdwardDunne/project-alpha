2d2ead488837f4bafd60b1551167a40ea220a23b
Edward P Dunne Jr. <edunne05@gmail.com>
2026-08-24 15:25:01 -0400

    Update usePersistedState.ts

--- app/frontend/src/hooks/usePersistedState.ts
+++ app/frontend/src/hooks/usePersistedState.ts
@@ -1,12 +1,17 @@
 import { Dispatch, SetStateAction, useCallback, useState } from "react"
 
-// same as useState except the state persists on component unmount.
+// Same as useState except the state persists on component unmount.
+// This hook was created to persist filter state but can be used generically.
+// This method was lighter weight than using redux, and, moving the filter's
+// state further up the tree wouldn't make sense since their closets parent
+// is HomePageRouter
 const persistedStateCache = new Map<string, unknown>()
 
 export function usePersistedState<T>(
     cacheKey: string,
     initialValue: T,
 ): [T, Dispatch<SetStateAction<T>>] {
+    // Using a function for state here so that it only runs once on initial render
     const [value, setValue] = useState<T>(() =>
         persistedStateCache.has(cacheKey)
             ? (persistedStateCache.get(cacheKey) as T)
